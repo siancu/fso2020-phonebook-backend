9d81a5d2cd9c067d4a7d23e96af64bb2b2dc3be2
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-08 19:37:05 +0200

    step 3

--- index.js
+++ index.js
@@ -38,6 +38,17 @@ app.get('/api/persons', (req, res) => {
   return res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    return res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
 const port = 3001
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
