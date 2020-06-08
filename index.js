b4b210e312d168219df79f1957e6352d30325e17
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-08 23:17:09 +0200

    step 8

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ const express = require('express')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('tiny'))
-
 app.use(express.json())
+
+//app.use(morgan('tiny'))
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    // req.method === 'POST' ? JSON.stringify(req.body) : ''
+    tokens.method(req, res) === 'POST' ? JSON.stringify(req.body) : ''
+  ].join(' ')
+}))
 
 let persons = [
   {
