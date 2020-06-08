96704eeff7b602eebc60984c6490790f93a54618
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-09 00:01:51 +0200

    Added the frontend.

---
+++ build/precache-manifest.6f68dd9bb38f0c7f0fdf4d32795f59d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e2456273ffccbb96ebeed4510a0502",
+    "url": "/index.html"
+  },
+  {
+    "revision": "900589b0b008c6488161",
+    "url": "/static/css/main.4abaeec6.chunk.css"
+  },
+  {
+    "revision": "e690b7896d4bc35c0816",
+    "url": "/static/js/2.ddec9da4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ddec9da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "900589b0b008c6488161",
+    "url": "/static/js/main.37b287b4.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 //app.use(morgan('tiny'))
 app.use(morgan((tokens, req, res) => {
