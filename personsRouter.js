9e41dd28a9092855d4d872747de12c77a3d642c8
Stelian Iancu <stelian.iancu@gmail.com>
2020-06-11 22:46:35 +0200

    phonebook 3.17 and 3.18

--- personsRouter.js
+++ personsRouter.js
@@ -3,18 +3,6 @@ const Person = require('./models/person')
 
 var router = express.Router()
 
-const errorHandler = (error, req, res, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
-  }
-
-  next(error)
-}
-
-router.use(errorHandler)
-
 const getPersons = async (req, res) => {
   const persons = await Person.find({})
   return res.json(persons)
